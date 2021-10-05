cf3c638ce2bceeec69f9ecf805425b9c2e1d34a4
Andres Romero <dev.jromero@gmail.com>
2021-10-04 23:48:14 -0500

    initial commit

---
+++ src/AppRouter.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const AppRouter:FC<any> = () => {
+    return (
+        <div className={'bg-blue-400'}>
+            My App
+        </div>
+    );
+}
+
+export default AppRouter;
