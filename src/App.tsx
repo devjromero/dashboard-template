c6f615207d2d0a19281627f9779f2a8a235d6793
Andres Romero <dev.jromero@gmail.com>
2021-10-04 23:58:10 -0500

    redux store config and sagas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import AppRouter from "./AppRouter";
+import store from "./store";
 
 function App() {
   return (
-    <AppRouter />
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
   );
 }
 
